89da6ed4528fa4f2cc1c2f1b4d4b8c7cf09bb716
Emren <93818242+EmrenBitirim@users.noreply.github.com>
2022-05-18 20:22:23 +0300

    Player animations added

---
+++ platformer_game_pt 2/states/TitleScreen.lua
@@ -0,0 +1,17 @@
+TitleScreenState = Class{__includes = BaseState}
+
+function TitleScreenState:init()
+
+end
+
+function TitleScreenState:render()
+
+end 
+
+function TitleScreenState:update(dt)
+
+end
+
+function TitleScreenState:render()
+
+end
